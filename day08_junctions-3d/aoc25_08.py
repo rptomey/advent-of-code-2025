fcb85f683a90d74f461e04347df0a05c75d803e6
Ryan Ptomey <rptomey6@gmail.com>
2025-12-08 15:24:24 -0600

    cleanup

--- day08_junctions-3d/aoc25_08.py
+++ day08_junctions-3d/aoc25_08.py
@@ -5,7 +5,6 @@ import re
 import networkx as nx
 import itertools
 from functools import reduce
-import copy
 
 # Add the parent directory to sys.path so we can find aoc_utils
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -14,9 +13,6 @@ from aoc_utils.distance_methods import point_to_point_distance as p_dist
 
 # To run, go to the folder in the terminal, and enter:
 # python <code-filename.py> <input-filename.txt>
-
-# Don't forget to use `copy.copy(thing)` or `copy.deepcopy(thing)`
-# to make changes to a thing without impacting the original version.
 
 def parse(file_name):
     """Parse input"""
@@ -81,7 +77,7 @@ def part1(data, path):
 
     return result
 
-def part2(data, path):
+def part2(data):
     """Solve part 2."""
     G = nx.Graph()
     uf = nx.utils.UnionFind()
@@ -115,7 +111,7 @@ def part2(data, path):
 def solve(puzzle_input, path):
     """Solve the puzzle for the given input."""
     solution1 = part1(puzzle_input, path)
-    solution2 = part2(puzzle_input, path)
+    solution2 = part2(puzzle_input)
 
     return solution1, solution2
 
