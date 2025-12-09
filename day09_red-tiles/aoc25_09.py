5826c25e210f9e4acd1f45af75b89ae6567019bd
Ryan Ptomey <rptomey6@gmail.com>
2025-12-09 13:12:16 -0600

    optimization for part 2 runtime

--- day09_red-tiles/aoc25_09.py
+++ day09_red-tiles/aoc25_09.py
@@ -39,33 +39,39 @@ def part1(data):
     """Solve part 1."""
     largest_area = 0
 
+    rect_areas = []
+
     for tile_a, tile_b in itertools.combinations(data, 2):
         area = rect_area(tile_a, tile_b)
+        rect_areas.append((tile_a, tile_b, area))
         if area > largest_area:
             largest_area = area
 
-    return largest_area
+    return largest_area, rect_areas
 
-def part2(data):
+def part2(data, rect_areas):
     """Solve part 2."""
     polygon = Polygon(data)
     largest_area = 0
 
-    for tile_a, tile_b in itertools.combinations(data, 2):
+    rect_areas = sorted(rect_areas, reverse=True, key=lambda x: x[2])
+
+    for rect_area in rect_areas:
+        tile_a = rect_area[0]
+        tile_b = rect_area[1]
         x1, x2 = sorted([tile_a[0], tile_b[0]])
         y1, y2 = sorted([tile_a[1], tile_b[1]])
         rectangle = box(minx=x1, miny=y1, maxx=x2, maxy=y2)
         if polygon.covers(rectangle):
-            area = rect_area(tile_a, tile_b)
-            if area > largest_area:
-                largest_area = area
+            largest_area = rect_area[2]
+            break
     
     return largest_area
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
-    solution1 = part1(puzzle_input)
-    solution2 = part2(puzzle_input)
+    solution1, rect_areas = part1(puzzle_input)
+    solution2 = part2(puzzle_input, rect_areas)
 
     return solution1, solution2
 
