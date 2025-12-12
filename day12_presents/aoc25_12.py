4fc31ba8a4d9bbad8ec3f4fae78d7fb616b8601a
Ryan Ptomey <rptomey6@gmail.com>
2025-12-12 09:54:10 -0600

    cleanup

--- day12_presents/aoc25_12.py
+++ day12_presents/aoc25_12.py
@@ -1,21 +1,9 @@
 import sys
-import os
 import time
 import re
-import copy
-
-# Add the parent directory to sys.path so we can find aoc_utils
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-# Import my helper functions, if needed
-from aoc_utils.span_methods import merge_spans
-from aoc_utils.distance_methods import point_to_point_distance as p_dist
 
 # To run, go to the folder in the terminal, and enter:
 # python <code-filename.py> <input-filename.txt>
-
-# Don't forget to use `copy.copy(thing)` or `copy.deepcopy(thing)`
-# to make changes to a thing without impacting the original version.
 
 def parse(file_name):
     """Parse input"""
